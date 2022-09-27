53e77e76386842cbc0083889b22ad6a5d6562a82
taward31 <taward31@gmail.com>
2022-09-27 19:33:15 +0000

    add some images to the quiz per question

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,46 +22,58 @@ if (qSeq >(0) ) {
 document.getElementById("bstart").style.display = "none";
 document.getElementById("aAnswer").style.display = "inline-block";
 document.getElementById("bAnswer").style.display = "inline-block";}
-document.getElementById("myImageId").src="newSource.png";
-
 
  
 
-switch (qSeq) {
+switch (qSeq) { 
 
     
     case 0:
         document.getElementById('question-text').textContent = "Welcome to the Tennis Grand Slam Quiz";
+        
+
         answer = 1 ; 
       break;
     case 1:
+        gCount = 0;
+        bCount = 0;
         document.getElementById('question-text').textContent = "Who is older Serena or Venus Williams ?  ";
         document.getElementById('aAnswer').textContent = "Serena";
         document.getElementById('bAnswer').textContent = "Venus";
+        document.getElementById("q-image").src = "assets/images/Ser_n_Venus.jpeg"
+        document.getElementById("q-image").alt = "Serena and Venus Williams ready to Play tennis prematch " 
         answer = 2; 
       break;
     case 2:
         document.getElementById('question-text').textContent = "40-40 is also know as ? ";
         document.getElementById('aAnswer').textContent = "Duece";
         document.getElementById('bAnswer').textContent = "Advantage";
+        document.getElementById("q-image").src = "assets/images/duece.jpeg"
+        document.getElementById("q-image").alt = "Two Tennis players in close match at Wimbledon" 
         answer = 1; 
       break;
     case 3:
        document.getElementById('question-text').textContent = "What surface is Wimbledon played on ? ";
        document.getElementById('aAnswer').textContent = "Clay";
        document.getElementById('bAnswer').textContent = "Grass";
+       document.getElementById("q-image").src = "assets/images/Wimbledon_logo.png" 
+       document.getElementById("q-image").alt = "Wimbledon Club Logo" 
        answer = 2; 
       break;
     case 4:
         document.getElementById('question-text').textContent = "An unreturned Serve is know as ? ";
         document.getElementById('aAnswer').textContent = "Ace";
         document.getElementById('bAnswer').textContent = "Jack";
+        document.getElementById("q-image").src = "assets/images/serve.jpeg"
+        document.getElementById("q-image").alt = "Pro tennis players in mid serve motion"  
         answer = 1; 
       break;
     case 5:
-        document.getElementById('question-text').textContent = "Which male player leads the Grand Slam race ? ";
+        document.getElementById('question-text').textContent = "Which player leads the Grand Slam race ? ";
         document.getElementById('aAnswer').textContent = "Novak Djokovic";
         document.getElementById('bAnswer').textContent = "Rafael Nadal ";
+        document.getElementById("q-image").src = "assets/images/rafa_novak.jpeg"
+        document.getElementById("q-image").alt = "Rafa and Novak ready to Play tennis prematch " 
         answer = 2; 
       break;
 
@@ -73,12 +85,11 @@ switch (qSeq) {
         document.getElementById("bAnswer").style.display = "none";
         document.getElementById('bstart').textContent = "Try Again ?";
         qSeq = 0 ;
-        gCount = 0;
-        bCount = 0;
+       
 
       break;
     
-    default :  alert ('Unknown Error Occurred - PLease Restart Game');
+    default :  alert ('Unknown Error Occurred - Please Restart Game');
 }}
 
 
